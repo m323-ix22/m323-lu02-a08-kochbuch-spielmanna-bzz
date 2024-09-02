34365149ddbdbe8a188e9b64fc89d1634ae87f08
andri <spielmanna@bzz.ch>
2024-09-02 10:24:09 +0200

    commit

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import json
 def adjust_recipe(recipe, num_people):
     """Adjusts the recipe ingredients based on the number of people."""
     factor = num_people / recipe['servings']
-    adjusted_ingredients = {ingredient: int(quantity * factor) for ingredient, quantity in recipe['ingredients'].items()}
+    adjusted_ingredients = {
+        ingredient: int(quantity * factor)
+        for ingredient, quantity in recipe['ingredients'].items()
+    }
     return {
-        "title": recipe['title'],
-        "ingredients": adjusted_ingredients,
-        "servings": num_people
+        'title': recipe['title'],
+        'ingredients': adjusted_ingredients,
+        'servings': num_people
     }
 
 def load_recipe(json_string):
@@ -18,13 +21,16 @@ def load_recipe(json_string):
 
 if __name__ == '__main__':
     # Example recipe JSON string
-    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
+    recipe_json = (
+        '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, '
+        '"Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
+    )
 
     # Load the recipe
-    recipe = load_recipe(recipe_json)
+    loaded_recipe = load_recipe(recipe_json)
 
     # Adjust the recipe for 2 people
-    adjusted_recipe = adjust_recipe(recipe, 2)
+    adjusted_recipe = adjust_recipe(loaded_recipe, 2)
 
     # Print the adjusted recipe
     print(json.dumps(adjusted_recipe, indent=4))
